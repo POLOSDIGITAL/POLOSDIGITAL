f79e775249a3b02304518dec025c37e78125e37c
POLOSDIGITAL <polosdigital07@gmail.com>
2026-09-16 21:08:23 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-// Smooth scroll animation
+```javascript
+// Firebase Authentication
+import { auth } from "./firebase-config.js";
 
-document.querySelectorAll('a[href^=\"#\"]').forEach(anchor => {
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+
+// CHECK LOGIN STATUS
+
+onAuthStateChanged(auth, (user) => {
+
+  if (!user) {
+
+    // User is NOT logged in
+    window.location.href = "login.html";
+
+    return;
+  }
+
+  // User is logged in
+  console.log("User logged in:", user.email);
+
+});
+
+
+// SMOOTH SCROLL
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
   anchor.addEventListener("click", function(e) {
 
@@ -14,3 +42,4 @@ document.querySelectorAll('a[href^=\"#\"]').forEach(anchor => {
   });
 
 });
+```
