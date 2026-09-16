1bde338e6e1abf5bc8c4d4dae1cc0c256570f6a8
POLOSDIGITAL <polosdigital07@gmail.com>
2026-09-16 21:36:34 +0530

    script.js

--- script.js
+++ script.js
@@ -1,45 +1,33 @@
-```javascript
 // Firebase Authentication
 import { auth } from "./firebase-config.js";
-
 import {
-  onAuthStateChanged,
-  signOut
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-
-// CHECK LOGIN STATUS
-
+// PROTECT HOME PAGE
 onAuthStateChanged(auth, (user) => {
-
   if (!user) {
-
-    // User is NOT logged in
-    window.location.href = "login.html";
-
+    // Not logged in → go to login page
+    window.location.href = "index.html";
     return;
   }
 
   // User is logged in
-  console.log("User logged in:", user.email);
-
+  console.log("User authenticated:", user.email);
 });
 
 
 // SMOOTH SCROLL
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-
   anchor.addEventListener("click", function(e) {
-
     e.preventDefault();
 
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({
+    const target = document.querySelector(this.getAttribute("href"));
+
+    if (target) {
+      target.scrollIntoView({
         behavior: "smooth"
       });
-
+    }
   });
-
 });
-```
